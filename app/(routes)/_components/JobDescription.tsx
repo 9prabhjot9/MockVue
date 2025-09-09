0247414e48c9854e09b6ec60ff7c33cbaca36d26
Prabhjot Singh <ps459811@gmail.com>
2025-09-09 16:43:57 +0530

    updated remuse jobdescription design

--- app/(routes)/_components/JobDescription.tsx
+++ app/(routes)/_components/JobDescription.tsx
@@ -6,12 +6,12 @@ function JobDescription() {
   return (
     <div className="border rounded-2xl p-10">
       <div>
-        <label className="text-black font-medium">Job Title</label>
-        <Input placeholder="Ex Full Stack Developer" />
+        <label className="text-black font-medium text-base ">Job Title</label>
+        <Input placeholder="Ex Full Stack Developer" className="text-black"/>
       </div>
       <div className="mt-6">
-        <label className="text-black font-medium" >Job Description</label>
-        <Textarea placeholder="Enter Job Description" className="h-[200px]" />
+        <label className="text-black font-medium text-base" >Job Description</label>
+        <Textarea placeholder="Enter Job Description" className="h-[200px] text-black" />
       </div>
     </div>
   );
